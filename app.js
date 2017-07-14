debfe8c198c5cc0365ea49dba8ea207ff46d3ea7
Sadaf Chowdhury <sadafchowdhury@gmail.com>
2017-07-14 01:24:58 -0400

    Added in methods from V1
    Added all the calculation methods from V1. Having issues with the JSON
    object in the getIndividualMatchJSONObj method.

--- app.js
+++ app.js
@@ -32,9 +32,14 @@ app.post('/', function(req, res) {
 
           var json = JSON.parse(body);
           var matchIDAndChampionIDData = getRankedMatchData(json); 
-         // var individualMatchData = getIndividualMatchJSONObj(matchIDAndChampionIDData);
-  
-          console.log(matchIDAndChampionIDData);
+          var individualMatchData = getIndividualMatchJSONObj(matchIDAndChampionIDData);
+        
+          console.log(individualMatchData);
+          //console.log(getWinRate(getIndividualMatchJSONObj));
+          //console.log(getAvgKDA(getIndividualMatchJSONObj));
+          //console.log(getWardingStat(getIndividualMatchJSONObj));
+          //console.log(getCreepScorePerMinDeltas(getIndividualMatchJSONObj));
+          //console.log(tiltalyzer(getIndividualMatchJSONObj));
 
           data.id = json['matches'][0].matchId;
           res.render('index', {test : data.id} );
@@ -59,7 +64,6 @@ function getRankedMatchData(json) {
         championID: [numberOfGames],
         lane: [numberOfGames]
     };
-    
     for (var i = 0; i < 3; i++) {
         matchData.matchID[i] = json['matches'][i].matchId;
         matchData.championID[i] = json['matches'][i].champion;
@@ -68,7 +72,108 @@ function getRankedMatchData(json) {
     return matchData;
 }
 
+function getIndividualMatchJSONObj(matchData) {
+    var matchParticipantData = {
+        specificParticipantData: [numberOfGames]
+    };
+    
+    for (var i = 0; i < numberOfGames; i++) {
+       matchParticipantData.specificParticipantData[i] =  getIndividualMatchJSONObjHelper(matchData, matchParticipantData, i);
+    }    
+    return matchParticipantData;
 
+}
+
+function getIndividualMatchJSONObjHelper(matchData, matchParticipantData, indexIter) {
+    var individualMatchURL = 'https://na1.api.riotgames.com/lol/match/v3/matches/' + matchData.matchID[indexIter] + '?api_key=' + API_KEY;
+    async.waterfall([
+            function(callback) {
+              request(individualMatchURL, function(err, response, body) {
+                if(!err && response.statusCode == 200) {
+                    var json = JSON.parse(body);
+                    for (var j = 0; j < 10; j++) {
+                        if (matchData.championID[indexIter] == json['participants'][j].championId) {
+                            return json['participants'][j];
+                            }
+                        }
+                    } else {
+                          console.log(err);
+                    }
+                });
+            }
+        ],
+        function(err, data) {
+            if(err) {
+                console.log(err);
+            return;
+            }
+        });
+    
+    return matchParticipantData;
+}
+
+function getWinRate(matchData) {
+    console.log(matchData);
+    winRate = 0;
+    finalWinRate = 0;
+    for (var i = 0; i < numberOfGames; i++) {
+        gameStatus = matchData.specificParticipantData[i].stats.winner;
+        if (gameStatus) {
+            winRate++;
+        }
+    }
+    finalWinRate = (winRate / numberOfGames) * 100;    
+    return finalWinRate;
+}
+
+function getAvgKDA(matchData) {
+    totalKills = totalDeaths = totalAssists = singleKDA =  averageKDA = 0;
+    for (var i = 0; i < numberOfGames; i++) {
+        totalKills = matchData.specificParticipantData[i].stats.kills;
+        totalDeaths = matchData.specificParticipantData[i].stats.deaths;
+        totalAssists = matchData.specificParticipantData[i].stats.assists;
+        singleKDA = (totalKills + totalAssists) / totalDeaths;
+        averageKDA += singleKDA
+    }
+    averageKDA = averageKDA / numberOfGames; 
+    
+    return Math.round(averageKDA * 100) / 100;
+}
+
+
+function getWardingStat(matchData) {
+    wardingStat = wardsPerGame =  0; 
+    for (var i = 0; i < numberOfGames; i++) {
+        wardingStat = matchData.specificParticipantData[i].stats.wardsPlaced;
+        wardsPerGame += wardingStat;
+    }
+    
+    wardsPerGame = wardsPerGame / numberOfGames;
+    return Math.round(wardsPerGame);
+}
+
+function getCreepScorePerMinDeltas(matchData) {
+    zeroToTenMinutesCSD = zeroToTenMinutesCSDeltaAverage = 0;
+    
+     for (var i = 0; i < numberOfGames; i++) {
+        zeroToTenMinutesCSD = matchData.specificParticipantData[i].timeline.creepsPerMinDeltas.zeroToTen;
+        zeroToTenMinutesCSDeltaAverage += zeroToTenMinutesCSD;
+    }
+    zeroToTenMinutesCSDeltaAverage = zeroToTenMinutesCSDeltaAverage / numberOfGames;
+    
+    return Math.round(zeroToTenMinutesCSDeltaAverage);
+
+}
+
+function tiltalyzer(winRate, avgKDA, wardingPerGame, csPerGameFirstTenMinutes) {
+    var tiltPercentage = 0;
+    winRate <= 50 ? tiltPercentage += .25 : tiltPercentage += 0; 
+    avgKDA <= 1.00 ? tiltPercentage += .25 : tiltPercentage += 0; 
+    wardingPerGame <= 20 ? tiltPercentage += .25 : tiltPercentage += 0; 
+    csPerGameFirstTenMinutes <= 5 ? tiltPercentage += .25 : tiltPercentage += 0; 
+    
+    return (tiltPercentage * 100);
+}
 var port = Number(process.env.PORT || 3000);
 app.listen(port);
     
